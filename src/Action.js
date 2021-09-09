f171250c2045394f1e92a38c14d4a9304da181a9
ErikoOzawa <eriko.o.1215@gmail.com>
2021-09-09 15:16:22 +0900

    カスタムコンポーネント

---
+++ src/Action.js
@@ -0,0 +1,41 @@
+
+import Cookies from "js-cookie";
+
+const Action = () => {
+    const mouseMove = (event) => {
+
+
+
+        //token update
+        const refresh_api_url = "http://localhost:3001/refresh";
+        const refreshTokenAndCookie = async () => {
+            const res = await fetch(refresh_api_url, {
+                method: "post",
+                mode: "cors",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: encodeURI(``),
+            });
+            const json = await res.json();
+            const message = json.message;
+            console.log(message);
+        }
+
+        //処理
+        if (Cookies.get('signedIn') === "true") {
+            refreshTokenAndCookie();
+            var in1Minutes = 1 / 1440;
+            Cookies.set("signedIn", "true", { expires: in1Minutes })
+        }
+
+    }
+
+
+    let mycanvas = document.getElementById('root');
+    mycanvas.addEventListener('mousemove', mouseMove);
+
+    return { mouseMove };
+}
+export default Action;
--- src/Private.js
+++ src/Private.js
@@ -1,9 +1,10 @@
 import { useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
+import Action from "./Action";
 
 const Private = () => {
-
+    const { mouseMove } = Action();
     const history = useHistory();
     // const [cookies, setCookie] = useCookies();
     const [content, setContent] = useState("loading...");
@@ -18,51 +19,22 @@ const Private = () => {
     }
 
     useEffect(() => {
-
-        const mouseMove = (event) => {
-            //private apiから情報を取得
-            const content_api_url = "http://localhost:3001/private";
-            const getPrivateContent = async () => {
-                const res = await fetch(content_api_url, {
-                    mode: "cors",
-                    credentials: "include"
-                });
-                const json = await res.json();
-                const content = json.message;
-                setContent(content);
-            }
-            //実行
-            getPrivateContent();
-
-            //token update
-            const refresh_api_url = "http://localhost:3001/refresh";
-            const refreshTokenAndCookie = async () => {
-                const res = await fetch(refresh_api_url, {
-                    method: "post",
-                    mode: "cors",
-                    credentials: "include",
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                    },
-                    body: encodeURI(``),
-                });
-                const json = await res.json();
-                const message = json.message;
-                console.log(message);
-            }
-
-            //処理
-            if (Cookies.get('signedIn') === "true") {
-                refreshTokenAndCookie();
-                var in1Minutes = 1 / 1440;
-                Cookies.set("signedIn", "true", { expires: in1Minutes })
-            }
-
+        //private apiから情報を取得
+        const content_api_url = "http://localhost:3001/private";
+        const getPrivateContent = async () => {
+            const res = await fetch(content_api_url, {
+                mode: "cors",
+                credentials: "include"
+            });
+            const json = await res.json();
+            const content = json.message;
+            setContent(content);
         }
 
+        //実行
+        getPrivateContent();
 
-        let mycanvas = document.getElementById('root');
-        mycanvas.addEventListener('mousemove', mouseMove);
+        mouseMove();
     }, []);
 
     return (
